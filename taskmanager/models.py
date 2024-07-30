a5a0923def73e163c0036ff0b2737cf00e6e191e
Arundhati <arundhatib.work@gmail.com>
2024-07-30 19:52:20 +0530

    task model

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
