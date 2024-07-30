086ea125ccd3bbe9178447c4f0065264c5e96742
Arundhati <arundhatib.work@gmail.com>
2024-07-30 21:20:36 +0530

    show all tasks

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def task_list(request):
+    tasks = Task.objects.all()
+    context = {
+        "tasks":tasks
+    }
+    return render(request, "taskmanager/task_list.html", context)
